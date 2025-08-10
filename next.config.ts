1b4ce3480cd31d14b8744e0b1e81187798382ee9
ShafayatAhammed <mdshafayat013@gmail.com>
2025-08-10 23:48:15 +0600

    revert: remove webpack path aliases config and explicit .tsx import

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,5 @@
 import type { NextConfig } from "next";
-import path from "path";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@app"] = path.resolve(__dirname, "src/app");
-    config.resolve.alias["@components"] = path.resolve(
-      __dirname,
-      "src/components"
-    );
-    config.resolve.alias["@constants"] = path.resolve(
-      __dirname,
-      "src/constants"
-    );
-    config.resolve.alias["@context"] = path.resolve(__dirname, "src/context");
-    config.resolve.alias["@fonts"] = path.resolve(__dirname, "src/fonts");
-    config.resolve.alias["@hooks"] = path.resolve(__dirname, "src/hooks");
-    config.resolve.alias["@lib"] = path.resolve(__dirname, "src/lib");
-    config.resolve.alias["@services"] = path.resolve(__dirname, "src/services");
-    config.resolve.alias["@styles"] = path.resolve(__dirname, "src/styles");
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
